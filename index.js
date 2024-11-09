a1a0ec0d778fe2110191a778f4d1fadf715bdcb2
Oussama <oussamafannah4@gmail.com>
2024-11-09 14:56:49 +0100

    commit 41

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express  from "express";
-import dotenv from  "dotenv";
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import { connectDB } from "./db/connectDB.js";
-import authRoutes from  "./routes/auth.route.js";
-import PostRoutes from  "./routes/post.route.js";
+import authRoutes from "./routes/auth.route.js";
+import PostRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js"
 import saveRoutes from "./routes/save.route.js"
 import SearchRoutes from "./routes/search.route.js"
@@ -12,22 +12,30 @@ import cors from 'cors';
 
 const app = express();
 dotenv.config();
-app.use(cors({ origin: true, credentials: true }));
+app.options('*', cors());
+// app.use(cors({
+//   origin: 'https://casadev-xqo7.vercel.app', // Replace with your frontend URL
+//   credentials: true, // Allow credentials (cookies, authorization headers)
+// }));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(cookieParser());
 app.use(express.json());
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 
-app.use("/api/auth", authRoutes) 
-app.use("/api/post", PostRoutes) 
-app.use("/api/search", SearchRoutes) 
+app.use("/api/auth", authRoutes)
+app.use("/api/post", PostRoutes)
+app.use("/api/search", SearchRoutes)
 app.use("/api/comment", commentRoutes)
 app.use("/api/save", saveRoutes)
 app.use("/api/follow", followRoutes)
 
 
 app.listen(PORT, () => {
-  connectDB();  
-  console.log("Server is running on port :",PORT);
+  connectDB();
+  console.log("Server is running on port :", PORT);
 });
 
 //dbcshAn7gU3RkSa5
