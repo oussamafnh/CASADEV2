15ab9498a6dd76daa2cc8a77a7338e94a3e2942d
Oussama <oussamafannah4@gmail.com>
2024-11-10 18:07:46 +0100

    commit 45

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express();
 dotenv.config();
 
 // app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-app.use(cors({ origin: 'https://casadev-cmog.vercel.app', credentials: true }));
-// app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: 'https://casadev-cmog.vercel.app', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(cookieParser());
 app.use(express.json());
